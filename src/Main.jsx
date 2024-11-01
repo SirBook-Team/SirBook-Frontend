4492b6c7d6d6658d73f9283acea0ecea2807dab0
Ibrahim_Adel <hima96326@gmail.com>
2024-11-01 04:20:33 +0200

    'jooooooooo'

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useContext } from 'react';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import profileImg from './images/profile.png';
+
 
 import { LoginContext } from './LoginContext';
+import { SidebarContext } from './SidebarContext';
 
 
 
@@ -10,63 +14,49 @@ const Main = () => {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    // const [postsApi, setPostsApi] = useState('https://ideal-computing-machine-wqqvr4qg96ghvgp7-4000.app.github.dev/api/posts');
-    const [postsApi, setPostsApi] = useState('https://jsonplaceholder.typicode.com/posts');
+    const [postsApi, setPostsApi] = useState('https://ideal-computing-machine-wqqvr4qg96ghvgp7-4000.app.github.dev/api/posts');
     const [isLoggedIn, setIsLoggedIn]= useContext(LoginContext);
+    const [isSidebarActive, setIsSidebarActive] = useContext(SidebarContext);
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+    const [checkTokenApi] = useState('https://ideal-computing-machine-wqqvr4qg96ghvgp7-4000.app.github.dev/api/auth');
+    const [deletePostApi, setDeletePostApi] = useState('');
 
 
-    // useEffect(() => {
-    //     fetch(api)
-    //         .then((response) => {
-    //             if (!response.ok) {
-    //                 throw new Error('Failed to fetch posts');
-    //             }
-    //             return response.json();
-    //         })
-    //         .then((data) => {
-    //             setPosts(data);
-    //             setLoading(false);
-    //         })
-    //         .catch((error) => {
-    //             setError(error.message);
-    //             setLoading(false);
-    //         });
-    // }, [api]);
+    useEffect(() => {
+        const checkToken = async () => {
+            if (isLoggedIn) {
+                const token = localStorage.getItem('token');
+                try {
+                    const response = await fetch(checkTokenApi, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${token}`, // send token as part of headers
+                        },
+                        // body: JSON.stringify({ token }),
+                    });
 
-    // console.log(posts);
-    // useEffect(() => {
-    //     const checkToken = async () => {
-    //         if (isLoggedIn) {
-    //             const token = localStorage.getItem('token');
-    //             try {
-    //                 const response = await fetch(checkTokenApi, {
-    //                     method: 'GET',
-    //                     headers: {
-    //                         'Content-Type': 'application/json',
-    //                         'Authorization': `Bearer ${token}`, // send token as part of headers
-    //                     },
-    //                     // body: JSON.stringify({ token }),
-    //                 });
+                    if (response.ok) {
+                        const data = await response.json();
+                        console.log(data);
+                        setUser(data);
+                    } else {
+                        setIsLoggedIn(false);
+                        localStorage.removeItem('token');
+                        navigate('/login');
+                    }
+                } catch (error) {
+                    console.error('Error verifying token:', error);
+                    setIsLoggedIn(false);
+                    localStorage.removeItem('token');
+                    navigate('/login');
+                }
+            }
+        };
+        checkToken();
+    }, [isLoggedIn, navigate, setIsLoggedIn, checkTokenApi]);
 
-    //                 if (response.ok) {
-    //                     const data = await response.json();
-    //                     console.log(data);
-    //                     setUser(data);
-    //                 } else {
-    //                     setIsLoggedIn(false);
-    //                     localStorage.removeItem('token');
-    //                     navigate('/login');
-    //                 }
-    //             } catch (error) {
-    //                 console.error('Error verifying token:', error);
-    //                 setIsLoggedIn(false);
-    //                 localStorage.removeItem('token');
-    //                 navigate('/login');
-    //             }
-    //         }
-    //     };
-    //     checkToken();
-    // }, [isLoggedIn, navigate, setIsLoggedIn, checkTokenApi]);
     useEffect(() => {
         const getPosts = async () => {
             if (isLoggedIn) {
@@ -96,19 +86,97 @@ const Main = () => {
         getPosts();
     }, [isLoggedIn, postsApi, setPosts]);
 
+    const viewPuplisherProfile = async (e) => {
+        // e.preventDefault();
+        const token = localStorage.getItem('token');
+        try{
+            const response = await fetch('',{
+                method:"POST",
+                headers:{
+                    // "Content-Type":"application/json",
+                    'Authorization': `Bearer ${token}`,
+                },
+                // body: JSON.stringify({ firstname, lastname, email, password, confirmPassword, gender, dateOfBirth, phoneNumber })
+                // body : formData,
+
+            });
+            console.log(response);
+            if(response.ok){
+                navigate('/profile');
+            }
+            else{
+                // alert('error');
+            }
+        }
+        catch(error){
+            console.error('Error: ', error);
+            alert('Server Error');
+        }
+    };
+
+    const deletePost = async (postId) => {
+        const token = localStorage.getItem('token');
+        setDeletePostApi(`https://ideal-computing-machine-wqqvr4qg96ghvgp7-4000.app.github.dev/api/posts/delete/${postId}`);
+        try{
+            const response = await fetch('',{
+                method:"POST",
+                headers:{
+                    // "Content-Type":"application/json",
+                    'Authorization': `Bearer ${token}`,
+                },
+                // body: JSON.stringify({ firstname, lastname, email, password, confirmPassword, gender, dateOfBirth, phoneNumber })
+                // body : formData,
+
+            });
+            console.log(response);
+            if(response.ok){
+                navigate('/profile');
+            }
+            else{
+                // alert('error');
+            }
+        }
+        catch(error){
+            console.error('Error: ', error);
+            alert('Server Error');
+        }
+    };
+
 
   return (
     <>
-        <section className={isLoggedIn ? 'bage-body' : 'bage'}>
-            <h1>Posts</h1>
-            <ul>
+        <section className={isSidebarActive ? 'bage-body active' : 'bage-body'}>
+            <div className='posts-box'>
                 {posts.map((post) => (
-                    <li key={post.id}>
-                        <h3>{post.title}</h3>
-                        <p>{post.body}</p>
-                    </li>
+                    <div key={post.id} className="post-box">
+                        <div>
+                            <button className='user-box' onClick={viewPuplisherProfile}>
+                                <img className='user-post-img' alt={ post ? `${post.owner.firstname} ${post.owner.lastname}'s profile` : 'profile'} src={ (post && post.owner && post.owner.profile) ? post.owner.profile : profileImg}/>
+                                <h3>{(post && post.owner) ? `${post.owner.firstname} ${post.owner.lastname}` : 'user\'s name'}</h3>
+                            </button>
+                            <button className='cross-btn' onClick={() => deletePost(post.id)}><i className="fas fa-times"></i></button>
+                        </div>
+                        <p>{(post && post.content) ? `${post.content}` : ``}</p>
+                        {(post && post.image) && <img className='post-img' alt={ post ? `${post.owner.firstname} ${post.owner.lastname}'s profile` : 'profile'} src={ post.image}/>}
+                    </div>
                 ))}
-            </ul>
+                {/* <div className='post-box'>
+                    <button className='user-box' onClick={viewPuplisherProfile}>
+                        <img className='user-post-img' alt={ user ? `${user.firstname} ${user.lastname}'s profile` : 'profile'} src={ user && user.profile ? user.profile : profileImg}/>
+                        <h3>{user ? `${user.firstname} ${user.lastname}` : 'user\'s name'}</h3>
+                    </button>
+                    <p>The evolution of technology has reshaped nearly every aspect of our lives, from how we communicate to how we work, learn, and entertain ourselves. The constant advancements in digital tools have made information and services more accessible, bridging gaps that once seemed insurmountable. Innovations like artificial intelligence, cloud computing, and mobile technology have redefined business models, enabling companies to reach global audiences and offer highly personalized experiences. However, with these benefits come challenges, including data privacy concerns and the need for digital literacy. As technology continues to evolve, it presents both opportunities and responsibilities, urging society to adopt these tools thoughtfully and ethically for a better future.</p>
+                    <img className='post-img' alt={ user ? `${user.firstname} ${user.lastname}'s profile` : 'profile'} src={ user && user.profile ? user.profile : profileImg}/>
+                </div>
+                <div className='post-box'>
+                    <button className='user-box' onClick={viewPuplisherProfile}>
+                        <img className='user-post-img' alt={ user ? `${user.firstname} ${user.lastname}'s profile` : 'profile'} src={ user && user.profile ? user.profile : profileImg}/>
+                        <h3>{user ? `${user.firstname} ${user.lastname}` : 'user\'s name'}</h3>
+                    </button>
+                    <p>The evolution of technology has reshaped nearly every aspect of our lives, from how we communicate to how we work, learn, and entertain ourselves. The constant advancements in digital tools have made information and services more accessible, bridging gaps that once seemed insurmountable. Innovations like artificial intelligence, cloud computing, and mobile technology have redefined business models, enabling companies to reach global audiences and offer highly personalized experiences. However, with these benefits come challenges, including data privacy concerns and the need for digital literacy. As technology continues to evolve, it presents both opportunities and responsibilities, urging society to adopt these tools thoughtfully and ethically for a better future.</p>
+                    <img className='post-img' alt={ user ? `${user.firstname} ${user.lastname}'s profile` : 'profile'} src={ user && user.profile ? user.profile : profileImg}/>
+                </div> */}
+            </div>
         </section>
     </>
   )
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -3,10 +3,10 @@ import { Link, useNavigate } from 'react-router-dom';
 import profileImg from './images/profile.png';
 
 import { LoginContext } from './LoginContext';
-// import { SidebarContext } from './SidebarContext';
+import { SidebarContext } from './SidebarContext';
 const Profile = () => {
     const [isLoggedIn, setIsLoggedIn]= useContext(LoginContext);
-    // const [isSidebarActive, setIsSidebarActive] = useContext(SidebarContext);
+    const [isSidebarActive, setIsSidebarActive] = useContext(SidebarContext);
     const [createPostApi] = useState('https://ideal-computing-machine-wqqvr4qg96ghvgp7-4000.app.github.dev/api/posts');
     const [checkTokenApi] = useState('https://ideal-computing-machine-wqqvr4qg96ghvgp7-4000.app.github.dev/api/auth');
     const navigate = useNavigate();
@@ -75,7 +75,38 @@ const Profile = () => {
                 navigate('/profile');
             }
             else{
-                alert(await response.text());
+                alert('error');
+            }
+        }
+        catch(error){
+            console.error('Error: ', error);
+            alert('Server Error');
+        }
+    };
+
+    const viewPuplisherProfile = async (e) => {
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+        const formData = new FormData();
+        formData.append('content', content);
+        if (image) formData.append('image', image);
+        try{
+            const response = await fetch(createPostApi,{
+                method:"POST",
+                headers:{
+                    // "Content-Type":"application/json",
+                    'Authorization': `Bearer ${token}`,
+                },
+                // body: JSON.stringify({ firstname, lastname, email, password, confirmPassword, gender, dateOfBirth, phoneNumber })
+                body : formData,
+
+            });
+            console.log(response);
+            if(response.ok){
+                navigate('/profile');
+            }
+            else{
+                alert('error');
             }
         }
         catch(error){
@@ -86,47 +117,68 @@ const Profile = () => {
 
   return (
     <>
-        <section className='bage-body '>
-            <form className='profile-container'>
-                <div className='profile-container'>
-                    <div className="main-box">
-                        <img alt={ user ? `${user.firstname} ${user.lastname}'s profile` : 'profile'} src={ user && user.profile ? user.profile : profileImg}/>
+        <section className={isSidebarActive ? 'bage-body active' : 'bage-body'}>
+            <form className='profile-form'>
+                <div className='flex-container left'>
+                    <div className='profile-container'>
+                        <div className="main-box">
+                            <img className='image' alt={ user ? `${user.firstname} ${user.lastname}'s profile` : 'profile'} src={ user && user.profile ? user.profile : profileImg}/>
+                        </div>
+                    </div>
+                    <h2>{user ? `${user.firstname} ${user.lastname}` : 'user\'s name'}</h2>
+                </div>
+                <div className='flex-container right'>
+                    <div className='flex-field'>
+                        <label className='view-label' htmlFor="firstname">firstname</label>
+                        <input type="text" name="firstname" value={user ? `${user.firstname}` : ''} required readOnly/>
+                    </div>
+                    <div className='flex-field'>
+                        <label className='view-label' htmlFor="lastname">lastname</label>
+                        <input type="text" name="lastname" value={user ? `${user.lastname}` : ''} required readOnly/>
+                    </div>
+                    <div className='flex-field'>
+                        <label className='view-label' htmlFor="email">email</label>
+                        <input type="email" name="email" value={user ? `${user.email}` : ''} required readOnly/>
+                    </div>
+                    <div className='flex-field'>
+                        <label className='view-label' htmlFor="dateOfBirth">birth date</label>
+                        <input type="date" name="dateOfBirth" value={user ? `${user.dateOfBirth}` : ''} required readOnly/>
+                    </div>
+                    <div className='flex-field'>
+                        <label className='view-label' htmlFor="phoneNumber">phone number</label>
+                        <input type="number" name="phoneNumber" value={user ? `${user.phoneNumber}` : ''} required readOnly/>
+                    </div>
+                    <div>
+                        <Link className='update-btn' to="/profile/update">update profile</Link>
                     </div>
                 </div>
-                <h2>{user ? `${user.firstname} ${user.lastname}` : 'user\'s name'}</h2>
-                <div className='flex-field'>
-                    <label className='view-label' htmlFor="firstname">firstname</label>
-                    <input type="text" name="firstname" value={user ? `${user.firstname}` : ''} required readOnly/>
-                </div>
-                <div className='flex-field'>
-                    <label className='view-label' htmlFor="lastname">lastname</label>
-                    <input type="text" name="lastname" value={user ? `${user.lastname}` : ''} required readOnly/>
-                </div>
-                <div className='flex-field'>
-                    <label className='view-label' htmlFor="email">email</label>
-                    <input type="email" name="email" value={user ? `${user.email}` : ''} required readOnly/>
-                </div>
-                <div className='flex-field'>
-                    <label className='view-label' htmlFor="dateOfBirth">birth date</label>
-                    <input type="date" name="dateOfBirth" value={user ? `${user.dateOfBirth}` : ''} required readOnly/>
-                </div>
-                <div className='flex-field'>
-                    <label className='view-label' htmlFor="phoneNumber">phone number</label>
-                    <input type="number" name="phoneNumber" value={user ? `${user.phoneNumber}` : ''} required readOnly/>
-                </div>
-                <div>
-                    <Link className='update-btn' to="/profile/update">update profile</Link>
-                </div>
             </form>
-            <form className='profile-container' onSubmit={createPost}>
+            <form className='create-post-form' onSubmit={createPost}>
                 <h2>Create new post</h2>
                 <label className='view-label' htmlFor="content">post content</label>
-                <input type="text" name="content" value={content} onChange={(e) => setcontent(e.target.value)} required/>
+                <textarea placeholder='description. . .' name="content" value={content} onChange={(e) => setcontent(e.target.value)} required maxLength='500' rows='5'/>
+                {/* <input type="text" name="content" value={content} onChange={(e) => setcontent(e.target.value)} required/> */}
                 <label htmlFor="image">upload image</label>
                 <input type="file" name="image" onChange={(e) => setImage(e.target.files[0])}/>
-                <input type="submit" value="Create"/>
+                <input className='create-post-btn' type="submit" value="Create"/>
             </form>
             <div className='posts-box'>
+                <div className='post-box'>
+                    <button className='user-box' onClick={viewPuplisherProfile}>
+                        <img className='user-post-img' alt={ user ? `${user.firstname} ${user.lastname}'s profile` : 'profile'} src={ user && user.profile ? user.profile : profileImg}/>
+                        <h3>{user ? `${user.firstname} ${user.lastname}` : 'user\'s name'}</h3>
+                    </button>
+                    <p>The evolution of technology has reshaped nearly every aspect of our lives, from how we communicate to how we work, learn, and entertain ourselves. The constant advancements in digital tools have made information and services more accessible, bridging gaps that once seemed insurmountable. Innovations like artificial intelligence, cloud computing, and mobile technology have redefined business models, enabling companies to reach global audiences and offer highly personalized experiences. However, with these benefits come challenges, including data privacy concerns and the need for digital literacy. As technology continues to evolve, it presents both opportunities and responsibilities, urging society to adopt these tools thoughtfully and ethically for a better future.</p>
+                    <img className='post-img' alt={ user ? `${user.firstname} ${user.lastname}'s profile` : 'profile'} src={ user && user.profile ? user.profile : profileImg}/>
+                </div>
+                <div className='post-box'>
+                    <button className='user-box' onClick={viewPuplisherProfile}>
+                        <img className='user-post-img' alt={ user ? `${user.firstname} ${user.lastname}'s profile` : 'profile'} src={ user && user.profile ? user.profile : profileImg}/>
+                        <h3>{user ? `${user.firstname} ${user.lastname}` : 'user\'s name'}</h3>
+                    </button>
+                    <p>The evolution of technology has reshaped nearly every aspect of our lives, from how we communicate to how we work, learn, and entertain ourselves. The constant advancements in digital tools have made information and services more accessible, bridging gaps that once seemed insurmountable. Innovations like artificial intelligence, cloud computing, and mobile technology have redefined business models, enabling companies to reach global audiences and offer highly personalized experiences. However, with these benefits come challenges, including data privacy concerns and the need for digital literacy. As technology continues to evolve, it presents both opportunities and responsibilities, urging society to adopt these tools thoughtfully and ethically for a better future.</p>
+                    <img className='post-img' alt={ user ? `${user.firstname} ${user.lastname}'s profile` : 'profile'} src={ user && user.profile ? user.profile : profileImg}/>
+                </div>
             </div>
         </section>
     </>
