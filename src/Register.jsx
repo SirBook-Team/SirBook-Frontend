319951447da3135a9d08f44fef121cc009fc8bcc
Ibrahim_Adel <hima96326@gmail.com>
2024-11-12 17:29:16 +0200

    edit routs in register bage

--- src/Register.jsx
+++ src/Register.jsx
@@ -55,7 +55,7 @@ const Register = () => {
                 }
             }
             else{
-                navigate('/');
+                navigate('/register');
             }
         };
         checkToken();
